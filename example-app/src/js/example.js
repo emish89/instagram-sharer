31e21fc75df4ec89724432853125a5cf3c60b197
Federico Ballarini <federico.ballarini.sc@gmail.com>
2025-09-10 22:35:43 +0200

    fix(name): fixed name and released

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { InstagramSharer } from 'instagram-sharer';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    InstagramSharer.echo({ value: inputValue })
-}
